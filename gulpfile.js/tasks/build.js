65043497a401aec4a8f5d4ecadd187c587a388dd
Leandro Kimura <leandro.kimura@mirumagency.com>
2017-07-14 16:10:03 -0300

    First commit

---
+++ gulpfile.js/tasks/build.js
@@ -0,0 +1,12 @@
+var gulp         = require('gulp');
+var gulpSequence = require('gulp-sequence');
+var gutil        = require('gulp-util');
+
+var buildTask = function (cb) {
+  gutil.env.type = 'prod';
+  gulpSequence('clean', 'html', 'images', 'css', 'js', 'fonts', cb);
+};
+
+// gulp build -> build for prod
+gulp.task('gulp:build', buildTask);
+module.exports = buildTask;
---
+++ gulpfile.js/tasks/css.js
@@ -0,0 +1,35 @@
+var config = require('../config');
+if(!config.tasks.css) return;
+
+var gulp      = require('gulp');
+var cssmin    = require('gulp-cssmin');
+var rename    = require('gulp-rename');
+var concat    = require('gulp-concat');
+var path      = require('path');
+
+var paths = {
+  src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
+  srcFonts: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.fontExtensions + '}'),
+  dest: path.join(config.root.dest, config.tasks.css.dest)
+};
+
+gulp.task('cssTask', function () {
+  gulp.src(paths.src)
+    .pipe(concat('style.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('cssVendorTask', function () {
+  gulp.src('./vendor/**/*.css')
+    .pipe(concat('vendor.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./assets/dist/css/vendor'));
+});
+
+gulp.task('cssFontsTask', function () {
+  gulp.src(paths.srcFonts)
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('css', ['cssTask','cssVendorTask','cssFontsTask']);
